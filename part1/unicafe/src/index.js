1b1e4eee9a4c05b78829e8c550e1bab65d72ce9d
MountVase <markus.vasemagi@gmail.com>
2020-08-03 21:24:25 +0300

    1.11 almost done

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -9,12 +9,11 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  
   return (
     <div>
       <Header text="give feedback" />
       <Button handleClick={() => setGood(good + 1)} text="good" />
-      <Button handleClick={() =>setNeutral(good + 1)}text="neutral" />
+      <Button handleClick={() =>setNeutral(neutral + 1)}text="neutral" />
       <Button handleClick={() => setBad(bad + 1)}text="bad" />
 
       <Statistics good={good} neutral={neutral} bad={bad} />
@@ -47,13 +46,22 @@ const Statistics = ({good, neutral, bad}) => {
 	<>
 	  <h1>statistics</h1>
 	  <table>
+     
 	  <Statistic text="good" count={good}/>
 	  <Statistic text="neutral" count={neutral}/>
 	  <Statistic text="bad" count={bad}/>
+    <Statistic text="all" count={good+neutral+bad} />
+    <Statistic text="average" count={average(good,neutral,bad)} />
+    <Statistic text="positive" count={positive(good, neutral, bad)} />
 	  </table>
 	</>
 	)
 }
+
+
+
+const average = (good, neutral, bad) => (good-bad)/(good+neutral+bad)
+const positive = (good, neutral, bad) => (good/(good+neutral+bad)*100).toString() + " %"
 
 
 ReactDOM.render(<App />, 
