a63fb5b5cd208a5fc65b346aa9cddfe78721ff21
MountVase <markus.vasemagi@gmail.com>
2020-09-02 20:03:59 +0300

    2.5

---
+++ part2/course-information/src/components/Course.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+
+
+// Structure should be 
+// App -> Course -> Header -> Content -> Part123 etc...
+
+const Course = ({ course }) => (
+    <>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </>
+  )
+  
+const Header = ({ course }) => <h2>{course}</h2>;
+  
+const Content = ({ parts }) => (
+    <>
+      {parts.map(({ name, exercises, id }) => (
+        <Part key={ id} name={name} exercises={exercises} />
+      ))}
+    </>
+  )
+  
+const Part = ({ name, exercises }) => (
+    <p>
+      {name} {exercises}
+    </p>
+  )
+  
+const Total = ({ parts }) => {
+    const total = parts.reduce((accel, currval) => accel + currval.exercises, 0)
+    return <p><b>total of {total} exercises</b> </p>;
+  }
+
+
+export default Course
