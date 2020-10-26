a7c9768baf053714a74e5899e605fe393203dc56
MountVase <markus.vasemagi@gmail.com>
2020-10-26 16:38:51 +0200

    configuration

--- part3/phonebook_backend/mongo.js
+++ part3/phonebook_backend/mongo.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-
+// configuring...
 if (process.argv.length<3) {
     console.log('Please provide the password as an argument: node mongo.js <password>')
     process.exit(1)
