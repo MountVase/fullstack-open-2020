ff07cafe7b124cf201fd757810c04293660581b7
MountVase <markus.vasemagi@gmail.com>
2020-10-27 15:57:14 +0200

    3.14, backend working, frontend pending

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,68 +1,63 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 
+const Person = require('./models/person')
+
 
 const app = express()
 
+//cors might be unnecessary
 app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
+
 //app.use(morgan('tiny'))
 
-const {PORT} = process.env.PORT || 3001
 
 
 morgan.token('info', (request, response) => JSON.stringify(request.body))
-
 app.use(morgan(':method :url :response-time ms :info'))
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
-
-let persons = [
- { id: 1, name: "Arto Hellas", number: "040-123456" },
- { id: 2, name: "Ada Lovelace", number: "39-44-54323523"},
- { id: 3, name: "Dan Abramov", number: "12-43-234345"},
- { id: 4, name: "Mary Poppendick", number: "39-23-6423122"}
-]
-
-
+/*
 const generateId = () => {
 	const maxId = persons.length > 0 
      	? Math.max(...persons.map(p => p.id))
  	: 0
      return maxId + 1
 }
+*/
 
 
 app.get('/api/persons', (request, response) => {
-   response.json(persons)
-
+	Person.find({}).then(persons => {
+		response.json(persons)
+	})
 })
+
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const perz = persons.find(p => p.id === id)
-
-    if (perz) {
-        response.json(perz)
-    } else {
-        response.status(404).end()
-    }
-
+	Person.findById(request.params.id).then(person => {
+		if (person) response.json(person.toJSON())
+		else response.status(404).end()
+	})
+	//doesn't work without error catching implementation, which comes later
+	//.catch(error => next(error))
 })
 
-
 app.get('/info', (request, response) => {
-   const info = `<p>Phonebook has info for ${persons.length} people </p>` 
+   Person.countDocuments({}).then(count => {
+
+   const info = `<p>Phonebook has info for ${count} people </p>` 
    
    const date = `<p>${new Date()}</p>`
    response.write(info)
    response.write(date)
 
    response.end()
+
+   })   
 })
 
 
@@ -75,23 +70,20 @@ app.post('/api/persons', (request, response) => {
      }
     // if name already exists
     // both http response (status) 400
-    else if (persons.map(p => p.name).includes(perz.name)) {
-        return response.status(400).json( { error: 'name must be unique' })
-    }
 
-    const realPers = {
-       name: perz.name, 
-       number: perz.number,
-       //id: Math.floor(Math.random() * 10000)
-       id: generateId()
-    }
+ 
+    const pers = new Person({
+    	name: perz.name,
+    	number: perz.number
+    })
 
-   persons = persons.concat(realPers)
+    pers.save().then(savedNote => {
+    	response.json(savedNote)
+    })
 
-   response.json(realPers)
 })
 
-
+/*
 app.delete('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
    // delete persons.id
@@ -99,11 +91,18 @@ app.delete('/api/persons/:id', (request, response) => {
    response.status(204).end()
 })
 
-
+*/
 
 //heroku crashes when GET '/'??
 app.get('/', (req, res) => {
 	res.write('<h1>welcome</h1>')
 	res.write('<p>you have been selected. figure out the next steps, if you are worthy')
 	res.end()
+})
+
+
+const PORT = process.env.PORT //|| 3001
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
 })
