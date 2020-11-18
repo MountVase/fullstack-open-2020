1a3f85818bdb058945b9cf49f152ebebf0c9faea
MountVase <markus.vasemagi@gmail.com>
2020-11-18 18:05:53 +0200

    4.8, 4.6 & 4.7 under construction

--- part4/bloglist/utils/config.js
+++ part4/bloglist/utils/config.js
@@ -2,8 +2,12 @@ require('dotenv').config()
 // handling of environment variables
 
 
-const MONGODB_URL = process.env.MONGODB_URL
+let MONGODB_URL = process.env.MONGODB_URL
 const PORT = process.env.PORT
+
+if (process.env.NODE_ENV === 'test') {
+	MONGODB_URL = process.env.TEST_MONGODB_URL
+}
 
 module.exports = {
      MONGODB_URL, 
