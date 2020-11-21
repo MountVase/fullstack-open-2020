6ff6279a144961501268cf6f6513e22f497f04e3
MountVase <markus.vasemagi@gmail.com>
2020-11-21 18:17:56 +0200

    4.21, token auth on deleting blogs too

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -54,8 +54,25 @@ blogsRouter.get('/:id', async (request, response) => {
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
-  const blog = await Blog.findByIdAndDelete(request.params.id)
-  response.status(204).end() //response.json(blog)
+  const body = request.body
+
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+
+  if (!request.token || !decodedToken.id || !decodedToken) {
+    return response.status(401).json({ error: 'token missing or invalid (loginstuff)' })
+  }
+
+  const user = await User.findById(decodedToken.id)
+  const blog = await Blog.findById(request.params.id)
+
+  if (blog.user.toString() === user._id.toString()) {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(201).end()
+  } else {
+    response.status(401).json({ error: 'cant just delete someone elses blog bro. Not cool bro.'})
+  }
+
+  //response.status(204).end() //response.json(blog)
 })
 
 blogsRouter.put('/:id', async (request, response) => {
