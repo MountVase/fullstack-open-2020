06d37b5bdbd619b188494c99d63ef4ba1567ac92
MountVase <markus.vasemagi@gmail.com>
2020-11-25 17:54:08 +0200

    5.1 login/blogs page working

---
+++ part5/bloglist-frontend/src/App.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import LoginForm from './components/LoginForm'
+import blogService from './services/blogs'
+import loginService from './services/loginService'
+
+
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs( blogs )
+    )  
+  }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      console.log(exception)
+    }
+  }
+
+  // handleLogin, username, setUsername, password, setPassword
+  if (user === null) {
+    const temp = <div><p>ass</p></div>
+    const t = <div><p>titties</p></div>
+
+    const loginform = <LoginForm
+                      onSubmit={handleLogin}
+                      username={username}
+                      setUsername={setUsername}
+                      password={password}
+                      setPassword={setPassword}
+                    ></LoginForm>
+
+    return (
+      loginform
+    )
+  }
+  
+  return (
+    <div>
+      <h2>blogs</h2>
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+    </div>
+  )
+}
+
+export default App
