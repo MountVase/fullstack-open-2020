20e12c8fd765032e08a622e174ee617150736eef
MountVase <markus.vasemagi@gmail.com>
2020-11-28 15:02:37 +0200

    5.14 add test for view button blog content

--- part5/bloglist-frontend/src/tests/Blogs.test.js
+++ part5/bloglist-frontend/src/tests/Blogs.test.js
@@ -5,18 +5,32 @@ import Blog from '../components/Blog'
 
 
 describe('Blog component & buttons tests', () => {
+  let blog
+  let component
+  let user
 
-  test('<Blog> initially renders title and author, but not url and likes', () => {
-    const blog = {
+  beforeEach(() => {
+    user = {
+      id: '5fbbb7bc9913156ae3911b59',
+      name: 'ForePlay',
+    }
+
+    blog = {
       title: 'testing just got a lot harder',
       author: 'jestPoppa',
       url: 'fuckTests.net',
-      user: '5fbbb7bc9913156ae3911b59'
+      likes: 420,
+      user: user
     }
-
-    const component = render(
-      <Blog blog={blog}></Blog>
+    component = render(
+      <Blog blog={blog} user={user}></Blog>
     )
+
+  })
+
+
+  test('<Blog> initially renders title and author, but not url and likes', () => {
+
     //component.debug()
     const preview = component.container.querySelector('.previewBlog')
     const afterview = component.container.querySelector('.completeBlog')
@@ -27,6 +41,14 @@ describe('Blog component & buttons tests', () => {
     expect(component.container).toHaveTextContent(blog.title, blog.author)
   })
 
+  test('After clicking show details button, blog url and likes are rendered', () => {
+    const button = component.getByText('view')
+    fireEvent.click(button)
 
+    const afterview = component.container.querySelector('.completeBlog')
+
+    expect(afterview).toBeDefined()
+    expect(afterview).toHaveTextContent(blog.url, blog.likes)
+  })
 })
 
