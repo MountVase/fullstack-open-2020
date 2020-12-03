a54735731fa0ce24b529a107816dc74ff41b2107
MountVase <markus.vasemagi@gmail.com>
2020-12-03 12:53:14 +0200

    7.1 add React BrowserRouter.

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -1,14 +1,19 @@
 import React, { useState } from 'react'
+import {
+  BrowserRouter as Router,
+  Switch, Route, Link
+} from "react-router-dom"
 
 const Menu = () => {
   const padding = {
     paddingRight: 5
   }
+
   return (
     <div>
-      <a href='#' style={padding}>anecdotes</a>
-      <a href='#' style={padding}>create new</a>
-      <a href='#' style={padding}>about</a>
+      <Link style={padding} to="/">home</Link>
+      <Link style={padding} to="/create">create new</Link>
+      <Link style={padding} to="/about">about</Link>
     </div>
   )
 }
@@ -125,13 +130,21 @@ const App = () => {
   return (
     <div>
       <h1>Software anecdotes</h1>
-      <Menu />
-      <AnecdoteList anecdotes={anecdotes} />
-      <About />
-      <CreateNew addNew={addNew} />
+      
+      <Router>
+      <Menu></Menu>
+        <Switch>
+          <Route path="/about"> <About></About> </Route>
+
+          <Route path="/create"> <CreateNew addNew={addNew}></CreateNew> </Route>
+
+          <Route path="/"> <AnecdoteList anecdotes={anecdotes}></AnecdoteList> </Route>
+        </Switch>
+      </Router>
+
       <Footer />
     </div>
   )
 }
 
-export default App;
+export default App
