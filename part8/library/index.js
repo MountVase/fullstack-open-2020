8ea061e3af23118cb068bb5205d73a02265ddd84
MountVase <markus.vasemagi@gmail.com>
2020-12-12 13:23:01 +0200

    8.5 add optional genre parameter to allBooks query.

--- part8/library/index.js
+++ part8/library/index.js
@@ -102,7 +102,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -111,7 +111,16 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: (root, args) => books.filter(b => b.author === args.author),
+    allBooks: (root, args) => {
+      if (args.author) {
+         return books.filter(b => b.author === args.author)
+      }
+
+      if (args.genre) {
+        return books.filter(b => b.genres.includes(args.genre))
+      }
+    
+    },
     allAuthors: () => authors
   },
 
