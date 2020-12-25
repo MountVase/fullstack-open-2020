ac3a9310a9ca0bf73bc67a82ee7aa4ecd8e91b1e
MountVase <markus.vasemagi@gmail.com>
2020-12-25 13:09:45 +0200

    9.17 add PatientPage view to each patient, add dispatcher.

---
+++ part9/patientor-frontend/src/components/PatientPage.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Divider, Container, Icon } from "semantic-ui-react";
+import { useParams } from 'react-router-dom';
+import { useStateValue } from "../state";
+import axios from 'axios';
+import { Patient } from '../types';
+import { apiBaseUrl } from '../constants';
+
+const PatientPage: React.FC = () => {
+
+    const [data, dispatch] = useStateValue();
+    const { id } = useParams<{ id: string }>();
+
+    React.useEffect(() => {
+        const fetcher = async () => {
+            const result = await axios.get<Patient>(`${apiBaseUrl}/patients/${id}`);
+            dispatch({ type: "SET_PATIENT_DETAILS" , payload: result.data });
+        };
+
+        if (!data?.patientDetail || data?.patientDetail?.id !== id) {
+            fetcher();
+        }
+    }, [data?.patientDetail]);
+    
+    const dict: any = { //eslint-disable-line
+        "male": "mars",
+        "female": "venus",
+        "other": "transgender alternate"
+    };
+
+    const genderIcon = dict[`${data?.patientDetail?.gender}`];
+    
+    return (
+        <div>
+            <Divider hidden />
+            <Container>
+                <h2>{data?.patientDetail?.name}</h2>
+                <Icon name={genderIcon} size="big"/>
+             </Container>
+             <Divider hidden />
+
+             <p><b>ssn: </b>{data?.patientDetail?.ssn}</p>
+             <p><b>occupation: </b>{data?.patientDetail?.occupation}</p>
+        </div>
+    );
+};
+
+export default PatientPage;
